d4e97fade853b8fa578f4d79e311faac135c0cf7
Stefan Collier <stefanjcollier@hotmail.com>
2020-11-23 20:04:44 +0000

    create environment based django settings

---
+++ dnd_dm_sidekick_api/settings/production.py
@@ -0,0 +1,14 @@
+import django_heroku
+from .base import *
+
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# must be last
+django_heroku.settings(locals())
