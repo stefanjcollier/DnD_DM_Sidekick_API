31070ad51f25273ad3b671fc4026940099bb57ee
Stefan Collier <stefanjcollier@hotmail.com>
2020-11-24 22:45:32 +0000

    Use postgres in production (heroku)

--- dnd_dm_sidekick_api/settings/production.py
+++ dnd_dm_sidekick_api/settings/production.py
@@ -1,13 +1,11 @@
 import django_heroku
+import dj_database_url
 from .base import *
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 INSTALLED_APPS += [
