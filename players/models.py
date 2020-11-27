bbbbb6fbcc7bd8cccfd918f5f62f33f9982bf4d2
Stefan Collier <stefanjcollier@hotmail.com>
2020-11-27 13:03:41 +0000

    add ReputationManager for Repuration
    that includes a method to find the defautl value

--- players/models.py
+++ players/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
 
+class ReputationManager(models.Manager):
+  def get_default(self):
+    self.get(charisma_modifier=0)
+
+
 class Reputation(models.Model):
+  objects = ReputationManager()
+
   name = models.CharField(max_length=240, null=False, blank=False)
   description = models.TextField(null=True, blank=True)
   charisma_modifier = models.IntegerField(verbose_name='The equivalent charisma boost', null=False, blank=False)
