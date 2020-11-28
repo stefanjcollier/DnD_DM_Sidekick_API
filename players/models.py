bbc374eae7765cac098b0883dabfd7764b16df84
Stefan Collier <stefanjcollier@hotmail.com>
2020-11-28 00:08:11 +0000

    include string methods on other models

--- players/models.py
+++ players/models.py
@@ -13,6 +13,9 @@ class Reputation(models.Model):
   description = models.TextField(null=True, blank=True)
   charisma_modifier = models.IntegerField(verbose_name='The equivalent charisma boost', null=False, blank=False)
 
+  def __str__(self):
+    return self.name
+
   class Meta:
     ordering = ['charisma_modifier']
 
@@ -21,3 +24,6 @@ class Character(models.Model):
   name = models.CharField(max_length=240, null=False, blank=False)
   charisma_modifier = models.IntegerField(null=False, blank=False)
   reputation = models.ForeignKey(Reputation, default=Reputation.objects.get_default().id, on_delete=models.SET_DEFAULT)
+
+  def __str__(self):
+    return self.name
