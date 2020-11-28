8b493cb55da7a47ccb8a9da7a22f89b0f3d933db
Stefan Collier <stefanjcollier@hotmail.com>
2020-11-28 00:06:08 +0000

    create a character view
    that uses two serializers

--- players/views.py
+++ players/views.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
+from dnd_dm_sidekick_api.library.mixins import MultiSerializerMixin
 
-from players.serializers import ReputationSerializer, AdminReputationSerializer
-from players.models import Reputation
+from players.serializers import (ReputationSerializer, AdminReputationSerializer,
+                                 CharacterCreationSerializer, CharacterViewSerializer)
+from players.models import Reputation, Character
 
 
 class ReputationView(viewsets.ModelViewSet):
@@ -12,3 +14,12 @@ class ReputationView(viewsets.ModelViewSet):
 class AdminReputationView(viewsets.ModelViewSet):
   serializer_class = AdminReputationSerializer
   queryset = Reputation.objects.all()
+
+
+class CharacterView(MultiSerializerMixin, viewsets.ModelViewSet):
+  serializer_classes = {
+    'list': CharacterViewSerializer,
+    'show': CharacterViewSerializer,
+  }
+  default_serializer_class = CharacterCreationSerializer
+  queryset = Character.objects.all()
